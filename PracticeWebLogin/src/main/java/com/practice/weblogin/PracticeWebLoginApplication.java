377a14393092aee307cc2044cd64470e771aa1ad
Yang tae wan <solme47@gmail.com>
2023-08-16 16:02:10 +0900

    0.0.1
    start from here

---
+++ PracticeWebLogin/src/main/java/com/practice/weblogin/PracticeWebLoginApplication.java
@@ -0,0 +1,13 @@
+package com.practice.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWebLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeWebLoginApplication.class, args);
+	}
+
+}
---
+++ PracticeWebLogin/src/test/java/com/practice/weblogin/PracticeWebLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.weblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeWebLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
